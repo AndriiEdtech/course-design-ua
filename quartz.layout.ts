84503c212b99ff5712e71ecbd86f68bbe9a5568e
R9R6G1 <andrygtfo@gmail.com>
2025-03-18 13:02:08 +0200

    Quartz sync: Mar 18, 2025, 1:02 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,6 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
       "AmzProfessional - лідер в e-commerce на Amazon ": "https://amzprof.team/",
     },
   }),
